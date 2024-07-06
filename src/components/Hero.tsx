b3cfb0b85804b97dc096d517655589fe83a35aab
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-06 18:17:23 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,6 @@
 import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const Hero = () => {
   return (
