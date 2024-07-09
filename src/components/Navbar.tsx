adda6913515b812de1aa84b3aff4e3ca8d0306db
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-09 11:02:46 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -81,7 +81,7 @@ export const Navbar = () => {
 
               <SheetContent side={"left"}>
                 <SheetHeader>
-                  <SheetTitle className="font-bold text-xl">Shadcn/React</SheetTitle>
+                  <SheetTitle className="font-bold text-xl">The Script App</SheetTitle>
                 </SheetHeader>
                 <nav className="flex flex-col justify-center items-center gap-2 mt-4">
                   {routeList.map(({ href, label }: RouteProps) => (
