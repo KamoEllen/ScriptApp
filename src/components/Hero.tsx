0f248a97e790a309607d4dd9a202b35b251a08d2
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-11 23:34:10 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,13 +26,13 @@ export const Hero = () => {
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
+          <Button className="w-full md:w-1/3">Download Now, Save Time Later</Button>
 
           <a
             className={`w-full md:w-1/3 }`}
           >
-            Get The Script App today
             
+            On-Set Solution
           </a>
         </div>
       </div>
