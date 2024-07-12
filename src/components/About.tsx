9099c33d325efe196a7e14a5b0675f3f3ad5bd8c
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-12 08:26:16 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,11 @@
-import about from "../assets/about-us-page-cuate.svg";
-
+import about from "../assets/connected-world-amico.svg";
 import { Statistics } from "./Statistics";
-//import pilot from "../assets/pilot.png";
 
 export const About = () => {
   return (
-    <section
-      id="about"
-      className="container py-24 sm:py-32"
-    >
-      <div className="bg-muted/50 border rounded-lg py-12">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+    <section id="about" className="container my-6 sm:my-8">
+      <div className="bg-white border rounded-lg py-12">
+        <div className="px-6 flex flex-col-reverse md:flex-row gap-6 md:gap-12">
           <img
             src={about}
             alt=""
@@ -20,48 +15,19 @@ export const About = () => {
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  Contact {" "}
+                  Contact Us
                 </span>
-                Us
               </h2>
               <p className="text-xl text-muted-foreground mt-4">
-                We’re here to help and answer any question you might have. We look forward to hearing from you.
+                We are here to help and answer any question you might have. We look forward to hearing from you.
+              </p>
+              <p className="text-sm text-gray-300 mt-2">
+                **We won't spam you, we promise!**
               </p>
             </div>
 
-            <Statistics />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-{/*
-import about from "../assets/About us page-cuate.svg";
-
-export const About = () => {
-  return (
-    <section id="about" className="container py-24 sm:py-32">
-      <div className="bg-muted/50 border rounded-lg py-5">
-        <div className="px-8 flex flex-col-reverse md:flex-row gap-4 md:gap-4">
-          <img
-            src={about}
-            alt=""
-            className="w-[400px] md:w-[300px] h-auto object-contain rounded-lg" // Adjusted image width
-          />
-          <div className="bg-green-0 flex flex-col justify-between">
-            <div className="pb-8 md:pb-0 pt-6"> 
-              <h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-4">
-                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
-                </span>
-                Let us know how we can help
-              </h2>
-              <p className="text-xl text-muted-foreground pt-6 mt-4 md:mt-4">
-                We’re here to help and answer any question you might have.We look forward to hearing from you .
-              </p>
-            
+            <div className="flex flex-col gap-4">
+              <Statistics />
             </div>
           </div>
         </div>
@@ -69,4 +35,4 @@ export const About = () => {
     </section>
   );
 };
-*/}
+
