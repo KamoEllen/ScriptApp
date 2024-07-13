b44e2f1f5c1d91b929ffe39758550a722f223d3d
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-13 13:35:05 +0200

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -80,7 +80,7 @@ export const Testimonials = () => {
       </h2>
 
       <p className="text-xl text-muted-foreground pt-4 pb-8">
-        Here are some of the testimonials from our satisfied customers.
+        Here is what other script supervisors have to say about their experience
       </p>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2 lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6">
