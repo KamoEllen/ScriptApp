6b8f3e064d8a1cd04efcb7a1714e3683c1f88ddd
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-22 15:28:52 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,6 +69,63 @@ export const Hero = () => {
             
             Download Now, Save Time Later
           </a>
+
+          {/* Ratings */}
+          <div class="flex space-x-4 items-center text-white">
+                        <div class="flex items-center space-x-2">
+                            <div class="flex flex-shrink-0 -space-x-1">
+                                <img loading="lazy" width="400" height="400" decoding="async"
+                                    class="h-6 w-6 max-w-none rounded-full ring-2 ring-white"
+                                    style="color:transparent" src="https://randomuser.me/api/portraits/men/29.jpg">
+                                <img loading="lazy" width="400" height="400" decoding="async"
+                                    class="h-6 w-6 max-w-none rounded-full ring-2 ring-white"
+                                    style="color:transparent" src="https://randomuser.me/api/portraits/men/90.jpg">
+                                <img loading="lazy" width="100" height="100" decoding="async"
+                                    class="h-6 w-6 max-w-none rounded-full ring-2 ring-white"
+                                    style="color:transparent" src="https://randomuser.me/api/portraits/men/75.jpg">
+                                <img loading="lazy" width="200" height="200" decoding="async"
+                                    class="h-6 w-6 max-w-none rounded-full ring-2 ring-white"
+                                    style="color:transparent" src="https://randomuser.me/api/portraits/men/5.jpg">
+                            </div>
+
+                            <span class="flex-shrink-0 text-xs font-medium leading-5">+15</span>
+
+                        </div>
+
+                        <div class="h-4 border-l border-gray-700"></div>
+
+                        <div class="flex items-center">
+
+                            <svg class="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 24 24">
+                                <path
+                                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z">
+                                </path>
+                            </svg>
+                            <svg class="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 24 24">
+                                <path
+                                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z">
+                                </path>
+                            </svg>
+                            <svg class="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 24 24">
+                                <path
+                                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z">
+                                </path>
+                            </svg>
+                            <svg class="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 24 24">
+                                <path
+                                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z">
+                                </path>
+                            </svg>
+                            <svg class="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 24 24">
+                                <path
+                                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z">
+                                </path>
+                            </svg>
+
+                        </div>
+                        
+                    </div>
+                                  {/*Ratings end */}
         </div>
       </div>
 
