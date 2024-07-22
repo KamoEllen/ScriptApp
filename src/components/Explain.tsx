1a115e4eb68e77f9aaa26a478e5732de5358c3f6
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-22 21:58:09 +0200

    Update Explain.tsx

--- src/components/Explain.tsx
+++ src/components/Explain.tsx
@@ -80,6 +80,29 @@ const Explain: React.FC = () => {
 
             {/* Popup */}
             {showPopup && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+        <div ref={popupRef} className="bg-white dark:bg-gray-800 rounded-lg overflow-hidden">
+            <button className="absolute top-2 right-2 text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white" onClick={closePopup}>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <div className="w-full h-full">
+                <iframe className="w-full h-full video-iframe" src={videoUrl} title="YouTube Video" allowFullScreen style={{ width: '800px', height: '450px' }} />
+            </div>
+        </div>
+    </div>
+)}
+
+           
+        </div>
+    );
+};
+
+export default Explain;
+
+ {/*
+            {showPopup && (
                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
                     <div ref={popupRef} className="bg-white dark:bg-gray-800 rounded-lg overflow-hidden">
                         <button className="absolute top-2 right-2 text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white" onClick={closePopup}>
@@ -93,8 +116,4 @@ const Explain: React.FC = () => {
                     </div>
                 </div>
             )}
-        </div>
-    );
-};
-
-export default Explain;
+            */}
