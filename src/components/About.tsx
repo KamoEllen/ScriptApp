eed85d332b453ef5a913af13b8e79576d9a2baf0
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-23 22:13:45 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ export const About = () => {
       
 
       <div className="container py-24 sm:py-32">
-      <div className="bg-white border rounded-lg py-12">
+      <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-6 md:gap-12">
           <img
             src={about}
