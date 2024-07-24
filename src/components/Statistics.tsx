6f61f3f78a4c14335981f4afa7a2c2255132de45
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-24 23:32:56 +0200

    Update Statistics.tsx

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -6,25 +6,7 @@ export const Statistics = () => {
   }
 
   const stats: StatsProps[] = [
-    {/*
-     {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-        </svg>
-      ),
-      quantity: "012 345 56788",
-      description: "Phone Number",
-    },
-    */},
+    
     {
       icon: (
         <svg
@@ -42,7 +24,25 @@ export const Statistics = () => {
       description: "Email",
     },
   ];
-
+{/*
+     {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+        </svg>
+      ),
+      quantity: "012 345 56788",
+      description: "Phone Number",
+    },
+    */}
   return (
     <section id="statistics" className="p-6 ">
       <div className="max-w-4xl mx-auto "> 
