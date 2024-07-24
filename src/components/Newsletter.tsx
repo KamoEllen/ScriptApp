c831ca27bd7cecfb9b492a75d9484cfe35ec8062
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-24 20:38:21 +0200

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -40,7 +40,7 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          Join Our {" "}
+          Join The {" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
             Community
           </span>
